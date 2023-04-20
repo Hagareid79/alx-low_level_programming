a79c1d7a14c1eca7ca7c4fb3e4ed22e4d8f4deb2
Hagar Eid <heid9723@gmail.com>
2023-04-20 06:45:24 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+/**
+ * main -entery
+ *
+ * disc c program
+ *
+ * return 0 always
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return 0 ;
+   puts("\"Programming is like building a multilingual puzzle");
+   return (0) ;
 }
