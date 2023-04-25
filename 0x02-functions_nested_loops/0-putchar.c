4e110c66f95703947e92641e5f477512ba0bbac4
Hagar Eid <heid9723@gmail.com>
2023-04-25 06:27:17 -0700

    0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{char s[] = "_putchar";
+for (int i = 0; i < 8; i++)
+{_putchar(s[i]);
+_putchar('\n');
+}
+return (0);
