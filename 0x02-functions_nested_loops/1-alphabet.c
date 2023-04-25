01cd2cfa1519592beb6c06ed7d218da34b31d500
Hagar Eid <heid9723@gmail.com>
2023-04-25 07:20:33 -0700

    f

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print alphabet -on the character function
+ *          to print from a to z
+*/
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+
+	_putchar('\n');
+}
