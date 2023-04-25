a1ed70f73298a703da93671b77b85e1a0e3b98c2
Hagar Eid <heid9723@gmail.com>
2023-04-25 08:43:24 -0700

    w

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _islower - function to chech
+ *@c: checks input f function
+ * Return: return 1 if lower else return 0 (Success)
+*/
+int _islower(int c)
+{
+if (c >= 97 && c <= 122)
+return (1);
+else
+return (0);
+}
