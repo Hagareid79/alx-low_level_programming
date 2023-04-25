66f43c006066d0a7271ce26a6813a4650a02dfec
Hagar Eid <heid9723@gmail.com>
2023-04-25 09:14:19 -0700

    u

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *_abs -function comput the absolute of number
+ *@n: tacks in integer type for function
+ *Return: Always 0 (Success)
+*/
+int _abs(int n)
+{
+if (n < 0)
+n = (-1) * n;
+return (n);
+}
+
