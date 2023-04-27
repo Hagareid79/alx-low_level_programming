a32272d5bd101618288a2f1b7d142f86d1760dee
Hagar Eid <heid9723@gmail.com>
2023-04-27 10:41:37 -0700

    c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,19 +8,17 @@ int main(void)
 int n;
 for (n = 1; n <= 100; n++)
 {
-if (n % 3 == 0 && !(n % 5 == 0))
-printf("Fizz");
-else if (n % 5 == 0 && !(n % 3 == 0))
-printf("Buzz");
-else if (n % 3 == 0 && n % 5 == 0)
+if (n % 15 == 0)
 printf("FizzBuzz");
+else if (n % 3 == 0)
+printf("Fizz");
+else if (n % 5 == 0)
+printf("Buzz");
 else
 printf("%d", n);
-
-if (n != 100)
+if (n < 100)
 printf(" ");
-else
-printf("\n");
 }
+printf("\n");
 return (0);
 }
