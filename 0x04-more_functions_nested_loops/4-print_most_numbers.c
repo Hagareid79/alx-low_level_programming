8f1949f73aaa185d1ada9bf9641078f347e7c5f9
Hagar Eid <heid9723@gmail.com>
2023-04-27 08:47:07 -0700

    k

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *print_most_numbers -print 0-9 without 2-4
+ *Return: Always 0 (Success)
+*/
+void print_most_numbers(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+if (i == 2 || i == 4)
+continue;
+_putchar(i + 48);
+}
+_putchar('\n');
+{
