14631952147321b8d67c253c70ec76f33814b72b
Hagar Eid <heid9723@gmail.com>
2023-04-27 09:23:15 -0700

    k

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *print_line -print a straight line
+ *@n: is the number of times i print
+*/
+void print_line(int n)
+{
+int i;
+if (n <= 0)
+_putchar('\n');
+else
+{
+for (i = 1; i <= n; i++)
+{
+	_putchar('_');
+}
+_putchar('\n');
+}
+}
