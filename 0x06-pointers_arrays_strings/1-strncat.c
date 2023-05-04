0e4aeae049f9cacf777dc58bad5b63dcf0b9f7ea
Hagar Eid <heid9723@gmail.com>
2023-05-04 04:07:16 -0700

    k

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -3,8 +3,8 @@
  *_strncat -that function is concate two string
  *@dest: is the first parameter
  *@src: is the second parameter
- *@n: is the third parameter
- *Return: pointer of concate string
+ *@n: is the third parameter, number of bytes from @src
+ *Return: @dest
 */
 char *_strncat(char *dest, char *src, int n)
 {
