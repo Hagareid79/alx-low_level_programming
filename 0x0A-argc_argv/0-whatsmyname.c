c5266ff4293b1718697d9ab9a1391246d98e2dbb
Hagar Eid <heid9723@gmail.com>
2023-05-12 04:18:37 -0700

    k

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - print the program name
+ * @a: integer
+ * @v: integer
+ * Return: ;
+*/
+int main(int a, char const *v[])
+{
+(void)a;
+printf("%s\n", a[0]);
+return (0);
+}
