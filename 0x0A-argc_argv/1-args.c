fa74d4df8504a0ec992e80680212e87b1c605569
Hagar Eid <heid9723@gmail.com>
2023-05-12 05:33:50 -0700

    l

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prints the number
+ * @argc: is a number
+ * @argv: is a number
+ * Return: zero
+*/
+int main(int argc, char *argv[])
+{
+(void)argv;
+printf("%d\n", argc - 1);
+return (0);
+}
