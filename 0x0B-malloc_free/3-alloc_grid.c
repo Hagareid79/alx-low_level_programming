7df2bb6352583e262619a7fb7478fe3e326e50f8
Hagar Eid <heid9723@gmail.com>
2023-05-16 13:30:54 -0700

    h

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,38 +1,45 @@
 #include "main.h"
 #include <stdlib.h>
-#include <stdio.h>
 /**
- * **alloc_grid - create two dim arr
- * @width: is a row
- * @height: is a column
- * Return: two dimention arr
+*alloc_grid - create a matrix using malloc
+*@width: width of the matrix
+*@height: height of the matrix
+* Return: return a pointer of pointer or null
 */
 int **alloc_grid(int width, int height)
 {
-int **arr;
-int x, y;
-if (height <= 0 || width <= 0)
-return (NULL);
-arr = (int **) malloc(sizeof(int *) * height);
-if (arr == NULL)
-return (NULL);
-for (x = 0; x < height; x++)
+int i, j, a, b;
+int **p;
+if (width <= 0 || height <= 0)
 {
-arr[x] = (int *) malloc(sizeof(int) * width);
-if (arr[x] == NULL)
-{
-free(arr);
-for (y = 0; y <= x; y++)
-free(arr[x]);
 return (NULL);
 }
-}
-for (x = 0; x < height; x++)
+else
 {
-for (y = 0; y < width; y++)
+p = (int *) malloc(height * sizeof(int *));
+if (!p)
 {
-arr[x][y] = 0;
+free(p);
+return (NULL);
+}
+for (i = 0; i < height; i++)
+{
+p[i] = (int *) malloc(width * sizeof(int));
+if (!p[i])
+{
+for (j = 0; j <= i; j++)
+free(p[j]);
+free(p);
+}
+return (NULL);
+}
+for (a = 0; a < height; a++)
+{
+for (b = 0; b < width; b++)
+{
+p[a][b] = 0;
 }
 }
-return (arr);
 }
+return (p);
+5}
