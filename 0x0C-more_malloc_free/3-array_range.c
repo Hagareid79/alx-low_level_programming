05a8a973c72ec7165923288bd15bde14ce905363
Hagar Eid <heid9723@gmail.com>
2023-05-18 14:02:38 -0700

    k

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ **array_range - allocates memory
+ * @min: is a integer
+ * @max: is an integer
+ * Return: pointer of array
+*/
+int *array_range(int min, int max);
+{
+int *x, y = 0;
+if (min > max)
+return (NULL);
+x = malloc((sizeof(int) * (max - min)) + sizeof(int));
+if (x == NULL)
+return (NULL);
+while (min <= max)
+{
+x[y] = min;
+y++;
+min++;
+}
+return (x);
+}
