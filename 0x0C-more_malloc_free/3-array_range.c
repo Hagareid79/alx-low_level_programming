e803a2f897a36dc9788018cd5a686146c0e4c75e
Hagar Eid <heid9723@gmail.com>
2023-05-18 14:30:06 -0700

    l

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,7 +1,5 @@
 #include "main.h"
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
+#include <stdlib.h>z
 /**
  **array_range - allocates memory
  * @min: is a integer
@@ -15,7 +13,7 @@ int *z;
 if (min > max)
 return (NULL);
 x = max - min + 1;
-z = malloc((sizeof(int) * x);
+z = malloc(sizeof(int) * x);
 if (!z)
 return (NULL);
 for (y = 0; y < x; y++)
