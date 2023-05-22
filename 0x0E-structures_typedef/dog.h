9f7fae70619c3ee7bf952ff274cd86827b385448
Hagar Eid <heid9723@gmail.com>
2023-05-22 07:25:57 -0700

    m

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - a dog
+ * @n: is a name of dog
+ * @a: is the age of dog
+ * @o: is owner,s name
+*/
+struct dog
+{
+char *n;
+float *a;
+char *o;
+}
+#endif
