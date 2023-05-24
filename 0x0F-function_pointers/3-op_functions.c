2d111ce68fc5447e9e842830e3dcf599afe203ad
Hagar Eid <heid9723@gmail.com>
2023-05-24 05:08:32 -0700

    l

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+/**
+ * op_add - add two num
+ * @a: first para
+ * @b: second para
+ * Return: the sum
+*/
+int op_add(int a, int b)
+{
+return (a + b);
+}
+/**
+ * op_sub - sub two num
+ * @a: first para
+ * @b: second para
+ * Return: the sum
+*/
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+/**
+ * op_mul - mult two num
+ * @a: first para
+ * @b: second para
+ * Return: the sum
+*/
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+/**
+ * op_div - div two num
+ * @a: first para
+ * @b: second para
+ * Return: the sum
+*/
+int op_div(int a, int b)
+{
+return (a / b);
+}
+/**
+ * op_mod - mod two num
+ * @a: first para
+ * @b: second para
+ * Return: the sum
+*/
+int op_mod(int a, int b)
+{
+return (a % b);
+}
