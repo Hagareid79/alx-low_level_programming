856b1bfecedca92de2e5cddb0635db76b30324d2
Hagar Eid <heid9723@gmail.com>
2023-05-24 03:39:02 -0700

    m

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+/**
+ * print_name - print a name
+ * @name: is a name
+ * @f: is function
+ * Return: void
+*/
+void print_name(char *name, void (*f)(char *))
+{
+if (name && f)
+f(name);
+}
